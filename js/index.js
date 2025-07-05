1a93117cf908d6a045426e251a7e3df1015c5034
Grisha55 <grishavinyar@icloud.com>
2025-07-05 18:27:48 +0300

    Refactored index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 import { data } from './data/data';
 import { Download } from './components/Download/Download.js';
+import { Warranty } from './components/Warranty/Warranty.js';
 
 const $root = document.getElementById('root');
 
 if ($root) {
   $root.insertAdjacentHTML('beforeend', Download(data.download));
+  $root.insertAdjacentHTML('beforeend', Warranty(data.warranty));
 }

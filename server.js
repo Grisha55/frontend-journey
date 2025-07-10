60c5e9fa96b8bb70b42548767c351989a43fad94
Grisha55 <grishavinyar@icloud.com>
2025-07-10 16:31:23 +0300

    Refctored server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import http from 'http';
-import { URL } from 'url';
-import { data } from './js/data/data.js';
+import { routes } from './js/routes/routes.js'
 
 const HOST = '127.0.0.1'
 const PORT = 3000;
@@ -14,36 +13,19 @@ const PORT = 3000;
 
 const handleServerRequest = (req, res) => {
   const endpoint = req.url ?? '';
-  const baseUrl = `http://${req.headers.host}`;
-  const url = new URL(endpoint, baseUrl);
+  console.log({ endpoint });
 
-  const isGetting = (
-    req.method === 'GET'
-  );
-  const isCurrentPath = (
-    url.pathname === '/api/data'
-  );
-  const headers = {
-    'Content-Type': 'application/json' ,
-    'Access-Control-Allow-Origin': '*'
-  };
-
-  if (isGetting && isCurrentPath) {
-    const resData = JSON.stringify(data);
-    const statusCode = 200;
-    res.writeHead(statusCode, headers).end(resData);
+  for (const route of routes) {
+    if (endpoint !== route.endpoint) return;
+    route.handler(req, res);
     return;
   }
-
-  const resData = JSON.stringify({'message': 'Not Found'});
-  const statusCode = 404;
-  res.writeHead(statusCode, headers).end(resData);
 };
 
-/**
- * @function handleServerListen
- * @returns {void}
- */
+// /**
+//  * @function handleServerListen
+//  * @returns {void}
+//  */
 
 const handleServerListen = () => {
   const url = `http://${HOST}:${PORT}`;

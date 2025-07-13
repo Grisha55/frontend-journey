59f2402f8ce1c947af61bc07faa7baa74405ae73
Grisha55 <grishavinyar@icloud.com>
2025-07-13 18:42:12 +0300

    Refactored Warranty.js

--- js/components/Warranty/Warranty.js
+++ js/components/Warranty/Warranty.js
@@ -5,27 +5,39 @@
 /**
  * @function Warranty
  * @description Large UI component
- * @param {Warranty} data 
- * @returns {string} HTML or empty
+ * @returns {Promise<string>} HTML or empty
  */
 
-export const Warranty = (data) => {
+export const Warranty = async () => {
 
-  if (!data) return '';
+  const API_URL = 'http://localhost:3000/data/warranty';
 
-  return /* html */`
-    <section class="warranty" id="warranty">
-      <div class="warranty__wrapper">
-        <div class="warranty__text-block">
-          <h2 class="warranty__title">${data.title.value}</h2>
-          ${data.texts.map ((text) => `<p class="warranty__copy">${text}</p>`).join('')}
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    /** @type {Warranty} */
+    const data = await response.json();
+    console.log(data);
+
+    return /* html */`
+      <section class="warranty" id="warranty">
+        <div class="warranty__wrapper">
+          <div class="warranty__text-block">
+            <h2 class="warranty__title">${data.title.value}</h2>
+            ${data.texts.map ((text) => `<p class="warranty__copy">${text}</p>`).join('')}
+          </div>
+          <div class="warranty__image-block">
+            <img 
+              src="${data.image.source}"
+              alt="${data.image.description}">
+          </div>
         </div>
-        <div class="warranty__image-block">
-          <img 
-            src="${data.image.source}"
-            alt="${data.image.description}">
-        </div>
-      </div>
-    </section>
-  `;
+      </section>
+    `;
+  } catch (error) {
+    console.error(error);
+    return '';
+  }
 };

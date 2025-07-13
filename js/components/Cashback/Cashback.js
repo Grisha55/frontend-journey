85955d9d619b4fe699f18711e817fbfc9b3d872a
Grisha55 <grishavinyar@icloud.com>
2025-07-13 18:40:55 +0300

    Refactored Cashback.js

--- js/components/Cashback/Cashback.js
+++ js/components/Cashback/Cashback.js
@@ -5,23 +5,35 @@
 /**
  * @function Cashback
  * @description Large UI component
- * @param {Cashback} data 
- * @returns {string} HTML or empty
+ * @returns {Promise<string>} HTML or empty
  */
 
-export const Cashback = (data) => {
+export const Cashback = async () => {
 
-  if (!data) return '';
+  const API_URL = 'http://localhost:3000/data/cashback';
+  
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    /** @type {Cashback} */
+    const data = await response.json();
+    console.log(data);
 
-  return /* html */`
-    <section class="cashback" id="cashback">
-      <div class="cashback__wrapper">
-        <div class="cashback__text-block">
-          <h2 class="cashback__title">${data.title.value}</h2>
-          ${data.texts.map ((text) => `<p class="cashback__copy">${text}</p>`).join('')}
-          <button class="cashback__button" type="${data.button.type}" id="order-open">${data.button.text}</button>
+    return /* html */`
+      <section class="cashback" id="cashback">
+        <div class="cashback__wrapper">
+          <div class="cashback__text-block">
+            <h2 class="cashback__title">${data.title.value}</h2>
+            ${data.texts.map ((text) => `<p class="cashback__copy">${text}</p>`).join ('')}
+            <button class="cashback__button" type="${data.button.type}" id="order-open">${data.button.text}</button>
+          </div>
         </div>
-      </div>
-    </section>
-  `;
+      </section>
+    `;
+  } catch (error) {
+    console.error(error);
+    return '';
+  }
 };

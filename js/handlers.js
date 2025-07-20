67b71f79f76cbde1e845834aae94b5dd944a7eaf
Grisha55 <grishavinyar@icloud.com>
2025-07-21 02:42:39 +0300

    Added handlers.js

---
+++ js/handlers.js
@@ -0,0 +1,26 @@
+import { generateSunIcon } from './ui/generateSunIcon.js';
+import { generateMoonIcon } from './ui/generateMoonIcon.js'
+
+const $app = document.querySelector('#app');
+const $themeButton = document.querySelector('#theme');
+
+/**
+ * Обработчик для переключения темы
+ * @param {Event} event 
+ */
+export const handleThemeButtonClick = (event) => {
+      const $themeButton = /** @type {HTMLElement} */ (event.currentTarget);
+      const theme = $themeButton.dataset.theme
+      if (theme === 'light') {
+        $themeButton.dataset.theme = 'dark';
+        $themeButton.innerHTML = generateSunIcon();
+        $app?.classList.add('dark');
+        $app?.classList.remove('light');
+      }
+      if (theme == 'dark') {
+        $themeButton.dataset.theme = 'light';
+        $themeButton.innerHTML = generateMoonIcon();
+        $app?.classList.add('light');
+        $app?.classList.remove('dark');
+      }
+    };

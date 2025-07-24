13dafa5f8cf65bfe09c0f39ad9bd20fe5cd34471
Grisha55 <grishavinyar@icloud.com>
2025-07-24 18:33:14 +0300

    Refactored server.js

--- server.js
+++ server.js
@@ -1,17 +1,53 @@
 import http from 'http';
-import { routes } from './js/routes/routes.js'
+import { routes } from './js/routes/routes.js';
 
 const HOST = '127.0.0.1'
 const PORT = 3000;
 
 /**
- * @function handleServerRequest
+ * @function setCorsHeaders
+ * @param {http.ServerResponse} res 
+ * @returns {void}
+ */
+
+const setCorsHeaders = (res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1.5500');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+};
+
+/**
+ * @function preflightRequestHandler
+ * @param {http.ServerResponse} res 
+ * @returns {void}
+ */
+
+const preflightRequestHandler = (res) => {
+  res.writeHead(204);
+  res.end();
+};
+
+/**
+ * @function findMatchingRoute
+ * @param {string} pathname 
+ * @param {string} method
+ * @returns {Route | undefined}
+ */
+
+const findMatchingRoute = (pathname, method) => {
+  return routes.find(route => 
+    pathname === route.endpoint && method === route.method
+  );
+};
+
+/**
+ * @function serverRequestHandler
  * @param {http.IncomingMessage} req
  * @param {http.ServerResponse} res
  * @returns {void}
  */
 
-const handleServerRequest = (req, res) => {
+const serverRequestHandler = (req, res) => {
   const endpoint = req.url ?? '';
   console.log({ endpoint });
 
@@ -22,10 +58,10 @@ const handleServerRequest = (req, res) => {
   }
 };
 
-// /**
-//  * @function handleServerListen
-//  * @returns {void}
-//  */
+/**
+ * @function handleServerListen
+ * @returns {void}
+ */
 
 const handleServerListen = () => {
   const url = `http://${HOST}:${PORT}`;
@@ -33,5 +69,5 @@ const handleServerListen = () => {
   console.log(message);
 };
 
-const server = http.createServer(handleServerRequest);
+const server = http.createServer(serverRequestHandler);
 server.listen(PORT, HOST, handleServerListen);

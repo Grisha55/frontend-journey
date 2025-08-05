7f879078fba8ca2f169d36cb114ced6f02458bb6
Grisha55 <grishavinyar@icloud.com>
2025-08-05 20:41:46 +0300

    Refactored Application

--- js/utils/index.js
+++ js/utils/index.js
@@ -1,6 +1,6 @@
 export { toCapitalize } from './toCapitalize/index.js';
 export { toggleNavigation } from './toggleNavigation/index.js';
-export { toggleModal } from './toggleModal/index.js';
+export { toggleModal } from './toggleModal/toggleModal.js';
 export { scrollToTarget } from './scrollToTarget/index.js';
 export { renderApp } from './renderApp/index.js';
 export { getData } from './getData/index.js';
